1c13f78bdb650f4fa26797c2e34598df3829d2bb
viraj.kolekar <viraj.kolekar@CWT-LPT-108.cirrius.com>
2025-09-01 12:00:46 +0530

    Final Commit

---
+++ JavaSessions/src/Java/ArraysConcept.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class ArraysConcept {
+
+	public static void main(String[] args) {
+		// Array - Non primitive data type
+		// it will only hold similar type of data
+		
+		//	new keyword
+		//array literals
+		
+		
+		
+		
+
+	}
+
+}
